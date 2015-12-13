e3baccc07682b556c02841d09263ad7d3b94384b
acemaster <vivekhtc25@gmail.com>
2015-12-14 02:40:27 +0530

    Download script almost complete

--- animedownload.py
+++ animedownload.py
@@ -1,13 +1,21 @@
 import requests
+import cfscrape
+from lxml import html
+import requests
+from bs4 import BeautifulSoup
+import json
+
 payload = {
-    'username': 'username',
-    'password': 'password'
+    'username': 'acemastervv',
+    'password': 'vivek123'
 }
+scraper = cfscrape.create_scraper()
 from subprocess import call
-while(True):
-	with requests.Session() as s:
-		s.mount("http://", requests.adapters.HTTPAdapter(max_retries=100))
-		s.mount("https://", requests.adapters.HTTPAdapter(max_retries=100))
-		s.post('https://kissanime.to/Login', data=payload)
-		r = s.get('https://kissanime.to/')
-		print r.content
+with requests.Session() as s:
+	s.mount("http://", requests.adapters.HTTPAdapter(max_retries=100))
+	s.mount("https://", requests.adapters.HTTPAdapter(max_retries=100))
+	s.post('https://kissanime.to/Login', data=payload)
+	page=scraper.get("https://kissanime.to/Anime/One-Punch-Man/Episode-011").content
+	soup = BeautifulSoup(page, 'html.parser')
+	div=soup.find_all('div', id='divDownload')
+	print div
